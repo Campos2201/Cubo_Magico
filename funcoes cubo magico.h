e8e3e5e0de38ce11c3fc41f4e1089e3b0c512540
isabelacrestana <crestanaisabela@gmail.com>
2025-09-01 16:42:13 -0300

    mudanca na representacao do cubo

--- funcoes cubo magico.h
+++ funcoes cubo magico.h
@@ -4,437 +4,182 @@
 #include <stdlib.h>
 
 // prototipos das funcoes
-void matrizResposta(int antiga[2][12]);
-void imprime(int matriz[2][12]);
-void copia(int orig[2][12], int dest[2][12]);
-void roda_lado_direito_para_cima(int matriz[2][12]);
-void roda_lado_direito_para_baixo(int matriz[2][12]);
-void roda_base_do_cubo_sentido_horario(int matriz[2][12]);
-void roda_base_do_cubo_sentido_antihorario(int matriz[2][12]);
-void roda_face_da_frente_do_cubo_sentido_horario(int matriz[2][12]);
-void roda_face_da_frente_do_cubo_sentido_antihorario(int matriz[2][12]);
-void roda_face_de_tras_do_cubo_sentido_horario(int matriz[2][12]);
-void roda_face_de_tras_do_cubo_sentido_antihorario (int matriz[2][12]);
-void roda_topo_do_cubo_sentido_horario (int matriz[2][12]);
-void esqCima(int antiga[2][12]);
-void cimaAntiHorario(int antiga[2][12]);
-void esqCimaNovo(int m[2][12]);
-void interfaceGrafica(int matriz[2][12]);
-void rotacionarAntiHorario(int matriz[2][12]);
-void rotacionarHorario(int matriz[2][12]);
-void rotacionarCima(int matriz[2][12]);
-void rotacionarBaixo(int matriz[2][12]);
-void mudarFace(int matriz[][12], int nova[][12], int faceAtual, int faceNova);
-void girarMatrizHorario(int matriz[][12], int faceAtual);
-void girarMatrizAntiHorario(int matriz[][12], int faceAtual);
-int face_montada(int matriz[][12], int face);
-int funcao_avaliadora(int matriz[][12]);
 
-
-void matrizResposta(int antiga[2][12])
+void vetorResposta(int v[24])
 {
-    int resposta[2][12] = {{1,2,5,6,9,10,13,14,17,18,21,22},
-                           {3,4,7,8,11,12,15,16,19,20,23,24}};
-
-    //int resposta[2][12] = {{1,1,2,2,3,3,4,4,5,5,6,6},
-    //                      {1,1,2,2,3,3,4,4,5,5,6,6}};
-
-    copia(resposta, antiga);
-}
-
-void imprime(int matriz[2][12])
-{
-    for(int i = 0; i<2; i++)
+    for(int i = 0; i<24; i++)
     {
-        for(int j = 0; j<12; j++)
-        {
-            printf("%02d ", matriz[i][j]);
-        }
-        printf("\n");
+        v[i] = i+1;
     }
-
 }
 
-void copia(int orig[2][12], int dest[2][12])
+void imprime(int vetor[24])
 {
-  for(int i = 0; i < 2; i++)
-  {
-      for(int j = 0; j < 12; j++)
-      {
-          dest[i][j] = orig[i][j];
-      }
-  }
+    for(int i = 0; i<24; i++)
+    {
+        printf("%02d ", vetor[i]);
+    }
 }
 
-void roda_lado_direito_para_cima(int matriz[2][12])
+void cld(int v[24])
 {
-    int aux1,aux2,aux3,aux4;
+    int aux,aux2;
 
-    aux1 = matriz[0][1];
-    aux2 = matriz[1][1];
-    matriz[0][1] = matriz[0][11];
-    matriz[1][1] = matriz[1][11];
-    aux3 = matriz[0][9];
-    aux4 = matriz[1][9];
-    matriz[0][9] = aux1;
-    matriz[1][9] = aux2;
-    aux1 = matriz[0][4];
-    aux2 = matriz[1][4];
-    matriz[0][4] = aux4;
-    matriz[1][4] = aux3;
-    matriz[0][11] = aux2;
-    matriz[1][11] = aux1;
-    aux1 = matriz[0][2];
-    aux2 = matriz[0][3];
-    aux3 = matriz[1][2];
-    aux4 = matriz[1][3];
-    matriz[0][2] = aux3;
-    matriz[0][3] = aux1;
-    matriz[1][2] = aux4;
-    matriz[1][3] = aux2;
+    aux = v[1];
+    v[1] = v[21];
+    aux2 = v[17];
+    v[17] = aux;
+    aux = v[10];
+    v[10] = aux2;
+    v[21] = aux;
 
+    aux = v[3];
+    v[3] = v[23];
+    aux2 = v[19];
+    v[19] = aux;
+
+    aux = v[8];
+    v[8] = aux2;
+    v[23] = aux;
+
+    aux = v[4];
+    v[4] = v[6];
+    aux2 = v[5];
+    v[5] = aux;
+    aux = v[7];
+    v[7] = aux2;
+    v[6] = aux;
 }
 
-void roda_lado_direito_para_baixo(int matriz[2][12])
-{
-    int aux1,aux2,aux3,aux4;
-
-    aux1 = matriz[0][1];
-    aux2 = matriz[1][1];
-    matriz[0][1] = matriz[0][9];
-    matriz[1][1] = matriz[1][9];
-    aux3 = matriz[0][11];
-    aux4 = matriz[1][11];
-    matriz[0][11] = aux1;
-    matriz[1][11] = aux2;
-    aux1 = matriz[0][4];
-    aux2 = matriz[1][4];
-    matriz[0][4] = aux4;
-    matriz[1][4] = aux3;
-    matriz[0][9] = aux2;
-    matriz[1][9] = aux1;
-    aux1 = matriz[0][3];
-    aux2 = matriz[1][3];
-    aux3 = matriz[0][2];
-    aux4 = matriz[1][2];
-    matriz[0][2] = aux1;
-    matriz[0][3] = aux2;
-    matriz[1][2] = aux3;
-    matriz[1][3] = aux4;
-
-}
-
-void roda_base_do_cubo_sentido_horario(int matriz[2][12])
-{
-    int aux1,aux2,aux3,aux4;
-
-    aux1 = matriz[0][10];
-    aux2 = matriz[0][11];
-    aux3 = matriz[1][10];
-    aux4 = matriz[1][11];
-    matriz[0][10] = aux2;
-    matriz[0][11] = aux4;
-    matriz[1][10] = aux1;
-    matriz[1][11] = aux3;
-    aux1 = matriz[1][0];
-    aux2 = matriz[1][1];
-    matriz[1][0] = matriz[1][2];
-    matriz[1][1] = matriz[1][3];
-    aux3 = matriz[1][6];
-    aux4 = matriz[1][7];
-    matriz[1][6] = aux1;
-    matriz[1][7] = aux2;
-    aux1 = matriz[1][4];
-    aux2 = matriz[1][5];
-    matriz[1][4] = aux3;
-    matriz[1][5] = aux4;
-    matriz[1][2] = aux1;
-    matriz[1][3] = aux2;
-
-
-}
-
-void roda_base_do_cubo_sentido_antihorario(int matriz[2][12])
-{
-    int aux1,aux2,aux3,aux4;
-
-    aux1 = matriz[1][0];
-    aux2 = matriz[1][1];
-    matriz[1][0] = matriz[1][6];
-    matriz[1][1] = matriz[1][7];
-    aux3 = matriz[1][2];
-    aux4 = matriz[1][3];
-    matriz[1][2] = aux1;
-    matriz[1][3] = aux2;
-    aux1 = matriz[1][4];
-    aux2 = matriz[1][5];
-    matriz[1][4] = aux3;
-    matriz[1][5] = aux4;
-    matriz[1][6] = aux1;
-    matriz[1][7] = aux2;
-    aux1 = matriz[0][10];
-    aux2 = matriz[0][11];
-    aux3 = matriz[1][10];
-    aux4 = matriz[1][11];
-    matriz[0][10] = aux3;
-    matriz[0][11] = aux1;
-    matriz[1][10] = aux4;
-    matriz[1][11] = aux2;
-
-}
-
-void roda_face_da_frente_do_cubo_sentido_horario(int matriz[2][12])
-{
-    int aux1,aux2,aux3,aux4;
-
-    aux1 = matriz[0][0];
-    aux2 = matriz[0][1];
-    aux3 = matriz[1][0];
-    aux4 = matriz[1][1];
-    matriz[0][0] = aux3;
-    matriz[0][1] = aux1;
-    matriz[1][0] = aux4;
-    matriz[1][1] = aux2;
-    aux1 = matriz[0][2];
-    aux2 = matriz[1][2];
-    matriz[0][2] = matriz[1][8];
-    matriz[1][2] = matriz[1][9];
-    aux3 = matriz[0][10];
-    aux4 = matriz[0][11];
-    matriz[0][10] = aux2;
-    matriz[0][11] = aux1;
-    aux1 = matriz[0][7];
-    aux2 = matriz[1][7];
-    matriz[0][7] = aux3;
-    matriz[1][7] = aux4;
-    matriz[1][8] = aux2;
-    matriz[1][9] = aux1;
-
-}
-
-void roda_face_da_frente_do_cubo_sentido_antihorario(int matriz[2][12])
-{
-    int aux1,aux2,aux3,aux4;
-
-    aux1 = matriz[0][0];
-    aux2 = matriz[0][1];
-    aux3 = matriz[1][0];
-    aux4 = matriz[1][1];
-    matriz[0][0] = aux2;
-    matriz[0][1] = aux4;
-    matriz[1][0] = aux1;
-    matriz[1][1] = aux3;
-    aux1 = matriz[0][2];
-    aux2 = matriz[1][2];
-    matriz[0][2] = matriz[0][11];
-    matriz[1][2] = matriz[0][10];
-    aux3 = matriz[1][8];
-    aux4 = matriz[1][9];
-    matriz[1][8] = aux1;
-    matriz[1][9] = aux2;
-    aux1 = matriz[0][7];
-    aux2 = matriz[1][7];
-    matriz[0][7] = aux4;
-    matriz[1][7] = aux3;
-    matriz[0][10] = aux1;
-    matriz[0][11] = aux2;
-
-}
-
-void roda_face_de_tras_do_cubo_sentido_horario(int matriz[2][12])
-{
-    int aux1,aux2,aux3,aux4;
-
-    aux1 = matriz[0][4];
-    aux2 = matriz[0][5];
-    aux3 = matriz[1][4];
-    aux4 = matriz[1][5];
-    matriz[0][4] = aux2;
-    matriz[0][5] = aux4;
-    matriz[1][4] = aux1;
-    matriz[1][5] = aux3;
-    aux1 = matriz[0][3];
-    aux2 = matriz[1][3];
-    matriz[0][3] = matriz[0][8];
-    matriz[1][3] = matriz[0][9];
-    aux3 = matriz[1][10];
-    aux4 = matriz[1][11];
-    matriz[1][10] = aux2;
-    matriz[1][11] = aux1;
-    aux1 = matriz[0][6];
-    aux2 = matriz[1][6];
-    matriz[0][6] = aux3;
-    matriz[1][6] = aux4;
-    matriz[0][8] = aux2;
-    matriz[0][9] = aux1;
-
-}
-void roda_face_de_tras_do_cubo_sentido_antihorario (int matriz[2][12])
-{
-    int aux1,aux2,aux3,aux4;
-
-    aux1 = matriz[0][4];
-    aux2 = matriz[0][5];
-    aux3 = matriz[1][4];
-    aux4 = matriz[1][5];
-    matriz[0][4] = aux3;
-    matriz[0][5] = aux1;
-    matriz[1][4] = aux4;
-    matriz[1][5] = aux2;
-    aux1 = matriz[1][10];
-    aux2 = matriz[1][11];
-    matriz[1][10] = matriz[0][6];
-    matriz[1][11] = matriz[1][6];
-    aux3 = matriz[0][3];
-    aux4 = matriz[1][3];
-    matriz[0][3] = aux2;
-    matriz[1][3] = aux1;
-    aux1 = matriz[0][8];
-    aux2 = matriz[0][9];
-    matriz[0][8] = aux3;
-    matriz[0][9] = aux4;
-    matriz[0][6] = aux2;
-    matriz[1][6] = aux1;
-
-}
-
-void roda_topo_do_cubo_sentido_horario (int matriz[2][12])
-{
-    int aux1,aux2,aux3,aux4;
-
-    aux1 = matriz[0][8];
-    aux2 = matriz[0][9];
-    aux3 = matriz[1][8];
-    aux4 = matriz[1][9];
-    matriz[0][8] = aux3;
-    matriz[0][9] = aux1;
-    matriz[1][8] = aux4;
-    matriz[1][9] = aux2;
-    aux1 = matriz[0][0];
-    aux2 = matriz[0][1];
-    matriz[0][0] = matriz[0][2];
-    matriz[0][1] = matriz[0][3];
-    aux3 = matriz[0][6];
-    aux4 = matriz[0][7];
-    matriz[0][6] = aux1;
-    matriz[0][7] = aux2;
-    aux1 = matriz[0][4];
-    aux2 = matriz[0][5];
-    matriz[0][4] = aux3;
-    matriz[0][5] = aux4;
-    matriz[0][2] = aux1;
-    matriz[0][3] = aux2;
-
-}
- // jeito diferente de implementar, acho q vou tirar dps
-void esqCima(int antiga[2][12])
-{
-    int nova[2][12];
-    copia(antiga, nova);
-
-    nova[0][0] = antiga[0][10];
-    nova[1][0] = antiga[1][10];
-
-    nova[0][5] = antiga[1][8];
-    nova[1][5] = antiga[0][8];
-
-    nova[0][6] = antiga[0][7];
-    nova[0][7] = antiga[1][7];
-
-    nova[1][6] = antiga[0][6];
-    nova[1][7] = antiga[1][6];
-
-    nova[0][8] = antiga[0][0];
-    nova[1][8] = antiga[1][0];
-
-    nova[0][10] = antiga[1][5];
-    nova[1][10] = antiga[0][5];
-
-    copia(nova, antiga);
-}
-
-void esqBaixo(int antiga[2][12])
+void bld(int v[24])
 {
     int aux1, aux2, aux3, aux4;
 
-    aux1 = antiga[0][0];
-    aux2 = antiga[1][0];
-    antiga[0][0] = antiga[0][8];
-    antiga[1][0] = antiga[1][8];
-    aux3 = antiga[0][10];
-    aux4 = antiga[1][10];
-    antiga[0][10] = aux1;
-    antiga[1][10] = aux2;
+    aux1 = v[4];
+    aux2 = v[5];
+    aux3 = v[6];
+    aux4 = v[7];
 
-    aux1 = antiga[0][5];
-    aux2 = antiga[1][5];
-    antiga[0][5] = aux4;
-    antiga[1][5] = aux3;
+    v[4] = aux2;
+    v[5] = aux4;
+    v[6] = aux1;
+    v[7] = aux3;
 
-    antiga[0][8] = aux2;
-    antiga[1][8] = aux1;
+    aux1 = v[1];
+    aux2 = v[3];
+    v[1] = v[17];
+    v[3] = v[19];
 
-    girarMatrizHorario(antiga,6);
+    aux3 = v[21];
+    aux4 = v[23];
+    v[21] = aux1;
+    v[23] = aux2;
 
+    aux1 = v[8];
+    aux2 = v[10];
+    v[8] = aux4;
+    v[10] = aux3;
+    v[17] = aux2;
+    v[19] = aux1;
 }
 
-void cimaAntiHorario(int antiga[2][12])
-{
-    int nova[2][12];
-    copia(antiga, nova);
-
-    nova[0][0] = antiga[0][6];
-    nova[0][1] = antiga[0][7];
-
-    nova[0][2] = antiga[0][0];
-    nova[0][3] = antiga[0][1];
-
-    nova[0][4] = antiga[0][2];
-    nova[0][5] = antiga[0][3];
-
-    nova[0][6] = antiga[0][4];
-    nova[0][7] = antiga[0][5];
-
-    nova[0][8] = antiga[0][9];
-    nova[0][9] = antiga[1][9];
-
-    nova[1][8] = antiga[0][8];
-    nova[1][9] = antiga[1][8];
-
-    copia(nova, antiga);
-}
-
-// movimento c base no jeito que o movimento oda_lado_direito_para_baixo foi implementado
-void esqCimaNovo(int m[2][12])
+void bsa(int v[24])
 {
     int aux1, aux2, aux3, aux4;
 
-    aux1 = m[0][0];
-    aux2 = m[1][0];
-    m[0][0] = m[0][10];
-    m[1][0] = m[1][10];
-    aux3 = m[0][8];
-    aux4 = m[1][8];
-    m[0][8] = aux1;
-    m[1][8] = aux2;
-    aux1 = m[0][5];
-    aux2 = m[1][5];
-    m[0][5] = aux4;
-    m[1][5] = aux3;
-    m[0][10] = aux2;
-    m[1][10] = aux1;
-    aux1 = m[0][6];
-    aux2 = m[0][7];
-    aux3 = m[1][6];
-    aux4 = m[1][7];
-    m[0][6] = aux2;
-    m[0][7] = aux4;
-    m[1][6] = aux1;
-    m[1][7] = aux3;
+    aux1 = v[20];
+    aux2 = v[21];
+    aux3 = v[22];
+    aux4 = v[23];
+
+    v[20] = aux3;
+    v[21] = aux1;
+    v[22] = aux4;
+    v[23] = aux2;
+
+    aux1 = v[2];
+    aux2 = v[3];
+    v[2] = v[14];
+    v[3] = v[15];
+
+    aux3 = v[6];
+    aux4 = v[7];
+
+    v[6] = aux1;
+    v[7] = aux2;
+
+    aux1 = v[10];
+    aux2 = v[11];
+    v[10] = aux3;
+    v[11] = aux4;
+    v[14] = aux1;
+    v[15] = aux2;
+}
+
+void bsh(int v[24])
+{
+    int aux1, aux2, aux3, aux4;
+
+    aux1 = v[20];
+    aux2 = v[21];
+    aux3 = v[22];
+    aux4 = v[23];
+
+    v[20] = aux2;
+    v[21] = aux4;
+    v[22] = aux1;
+    v[23] = aux3;
+
+    aux1 = v[2];
+    aux2 = v[3];
+    v[2] = v[6];
+    v[3] = v[7];
+
+    aux3 = v[14];
+    aux4 = v[15];
+    v[14] = aux1;
+    v[15] = aux2;
+
+    aux1 = v[10];
+    aux2 = v[11];
+    v[10] = aux3;
+    v[11] = aux4;
+
+    v[6] = aux1;
+    v[7] = aux2;
+}
+
+void tsa(int v[24])
+{
+    int aux1, aux2, aux3, aux4;
+    aux1 = v[8];
+    aux2 = v[9];
+    aux3 = v[10];
+    aux4 = v[11];
+
+    v[8] = aux3;
+    v[9] = aux1;
+    v[10] = aux4;
+    v[11] = aux2;
+
+    aux1 = v[22];
+    aux2 = v[23];
+    v[22] = v[12];
+    v[23] = v[14];
+    aux3 = v[5];
+    aux4 = v[7];
+    v[5] = aux2;
+    v[7] = aux1;
+    aux1 = v[16];
+    aux2 = v[17];
+    v[16] = aux3;
+    v[17] = aux4;
+    v[12] = aux2;
+    v[14] = aux1;
 }
 
 // Funcao que imprime o cubo para o jogador
-void interfaceGrafica(int matriz[2][12])
+void interfaceGrafica(int v[24])
 {
     printf("                        +---------+ \n"
            "                       / %02d   %02d / |\n"
@@ -445,147 +190,86 @@ void interfaceGrafica(int matriz[2][12])
            "                    |         |   /\n"
            "                    | %02d   %02d |%02d/\n"
            "                    +---------+ /\n",
-           matriz[0][8], matriz[0][9],
-           matriz[0][3], matriz[1][8],
-           matriz[1][9], matriz[0][2],
-           matriz[0][0], matriz[0][1],
-           matriz[1][3], matriz[1][0],
-           matriz[1][1], matriz[1][2]);
+           v[16], v[17],
+           v[5], v[18],
+           v[19], v[4],
+           v[0], v[1],
+           v[7], v[2],
+           v[3], v[6]);
 }
 
-void rotacionarAntiHorario(int matriz[2][12])
+int face_montada(int v[24], int face)
 {
-    int nova[2][12];
-    copia(matriz, nova);
-
-    for(int i=0; i<6; i=i+2)
+    for(int i = 0; i<3; i++)
     {
-        mudarFace(matriz,nova,i,2); // "shift-left"
+        if(v[i]!=v[i+1])
+            return 0;
     }
-
-    mudarFace(matriz,nova,6,-6);
-    copia(nova, matriz);
-
-    girarMatrizAntiHorario(matriz, 8); // Face de cima
-    girarMatrizHorario(matriz, 10); // Face de baixo
-
-}
-
-void rotacionarHorario(int matriz[2][12])
-{
-    int nova[2][12];
-    copia(matriz, nova);
-
-    mudarFace(matriz, nova, 2, -2);
-    mudarFace(matriz, nova, 4, -2);
-    mudarFace(matriz, nova, 6, -2);
-    mudarFace(matriz, nova, 0, 6);
-
-    girarMatrizHorario(nova, 8);
-    girarMatrizAntiHorario(nova, 10);
-
-    copia(nova, matriz);
-}
-
-void rotacionarCima(int matriz[2][12])
-{
-    int nova[2][12];
-    copia(matriz, nova);
-
-    mudarFace(matriz, nova, 10, -10);
-
-    girarMatrizHorario(nova, 2);
-
-    girarMatrizAntiHorario(matriz, 8);
-    girarMatrizAntiHorario(matriz, 8);
-    mudarFace(matriz, nova, 8, -4);
-
-    girarMatrizAntiHorario(nova, 6);
-
-    mudarFace(matriz, nova, 0, 8);
-
-    girarMatrizHorario(matriz, 4);
-    girarMatrizHorario(matriz, 4);
-    mudarFace(matriz, nova, 4, 6);
-
-    copia(nova, matriz);
-}
-
-void rotacionarBaixo(int matriz[2][12])
-{
-    int nova[2][12];
-    copia(matriz, nova);
-
-    mudarFace(matriz, nova,8, -8);
-
-    girarMatrizAntiHorario(nova, 2);
-
-    girarMatrizAntiHorario(matriz, 10);
-    girarMatrizAntiHorario(matriz, 10);
-    mudarFace(matriz, nova, 10, -6);
-
-    girarMatrizHorario(nova, 6);
-
-    girarMatrizHorario(matriz, 4);
-    girarMatrizHorario(matriz, 4);
-    mudarFace(matriz, nova, 4, 4);
-
-    mudarFace(matriz, nova, 0, 10);
-
-    copia(nova, matriz);
-}
-
-void mudarFace(int matriz[][12], int nova[][12], int faceAtual, int faceNova)
-{
-    for(int i=0; i<2; i++)
-    {
-        for(int j=faceAtual; j<faceAtual+2; j++)
-        {
-            nova[i][j+faceNova] = matriz[i][j];
-        }
-    }
-}
-
-void girarMatrizAntiHorario(int matriz[][12], int faceAtual)
-{
-    int aux = matriz[0][faceAtual];
-    matriz[0][faceAtual] = matriz[0][faceAtual+1];
-    matriz[0][faceAtual+1] = matriz[1][faceAtual+1];
-    matriz[1][faceAtual+1] = matriz[1][faceAtual];
-    matriz[1][faceAtual] = aux;
-}
-
-void girarMatrizHorario(int matriz[][12], int faceAtual) // ver dps
-{
-    int aux = matriz[0][faceAtual];
-    matriz[0][faceAtual] = matriz[1][faceAtual];
-    matriz[1][faceAtual] = matriz[1][faceAtual + 1];
-    matriz[1][faceAtual + 1] = matriz[0][faceAtual + 1];
-    matriz[0][faceAtual + 1] = aux;
-}
-
-int face_montada(int matriz[][12], int face)
-{
-    if(matriz[0][face] != matriz[0][face])
-        return 0;
-    if(matriz[0][face] != matriz[1][face])
-        return 0;
-    if(matriz[0][face] != matriz[1][face+1])
-        return 0;
 
     return 1;
 }
 
-int funcao_avaliadora(int matriz[][12])
+int funcao_avaliadora(int v[24])
 {
-    for(int i=0; i<8; i+=4)
+    for(int i=0; i<20; i+=4)
     {
         // ja retorna caso ache uma face que nao esteja montada
-        if(!face_montada(matriz,i))
+        if(!face_montada(v,i))
             return 0;
     }
 
     return 1; // cubo montado
+}
+
+void realiza_mov(int mov, int v[24])
+{
+    switch(mov)
+    {
+        case 1:
+        {
+            cld(v);
+            break;
+        }
+
+        case 2:
+        {
+            bld(v);
+            break;
+        }
+
+        case 3:
+        {
+            bsa(v);
+            break;
+        }
+
+        case 4:
+        {
+            bsh(v);
+            break;
+        }
+
+        case 5:
+        {
+            break;
+        }
+
+        case 6:
+        {
+            tsa(v);
+            break;
+        }
+
+        case 7:
+        {
+            break;
+        }
+
+        case 0:
+        {
+            break;
+        }
+    }
 }
 
 #endif // FUNCOES_CUBO_MAGICO_H_INCLUDED
--- main.c
+++ main.c
@@ -5,215 +5,31 @@
 #include "funcoes cubo magico.h"
 
 void menu(int matriz[][12]);
-int opcoesJogador(int matriz[2][12]);
+int opcoesJogador(int v[24]);
 int aleatorio(int n_min, int n_max);
-int cuboInicial(int matriz[][12]);
-void realiza_mov(int mov, int matriz[][12]);
+int cuboInicial(int v[24]);
 
 int main() {
     srand(time(NULL)); // Inicializa o gerador com o tempo atual
 
-    int cubo_magico[2][12];
+    int cubo_magico[24];
+    int opc;
 
     // iniciando como matriz resposta so para comparar:
-    matrizResposta(cubo_magico);
+    vetorResposta(cubo_magico);
     printf("\n\nCubo antes de embaralhar:\n");
     interfaceGrafica(cubo_magico);
 
-    //printf("\n\nCubo depois de embaralhar\n");
+    printf("\n\nCubo depois de embaralhar\n");
     //cuboInicial(cubo_magico);
-    //interfaceGrafica(cubo_magico);
+    interfaceGrafica(cubo_magico);
 
     printf("\n\npressione qualquer tecla...");
     getch();
     system("cls");
 
-    menu(cubo_magico);
-    printf("\n\n         Voce venceu :) !\n");
-    interfaceGrafica(cubo_magico);
-
-    /*int matriz_resposta[2][12];
-
-    // construindo a matriz resposta
-    matrizResposta(matriz_resposta);
-
-    // copiando a matriz resposta no cubo magico, para testes
-    copia(matriz_resposta, cubo_magico);
 
 
-    // imprimindo matriz resposta
-    printf("Matriz resposta: \n");
-    imprime(matriz_resposta);
-    printf("\n");
-
-    roda_lado_direito_para_baixo(cubo_magico);
-
-    // imprimindo cubo magico
-    printf("Cubo magico: \n");
-    imprime(cubo_magico);
-
-    roda_lado_direito_para_cima(cubo_magico);
-
-    // imprimindo cubo magico
-    printf("Cubo magico: \n");
-    imprime(cubo_magico);
-
-    printf("Interface grafica do cubo atual:\n");
-    interfaceGrafica(cubo_magico);
-    */
-
-    return 0;
-}
-
-int aleatorio(int n_min, int n_max)
-{
-    return rand() % (n_max - n_min + 1) + n_min;
-}
-
-// funcao que embaralha o cubo a partir da matriz resposta
-int cuboInicial(int matriz[][12])
-{
-    int anterior = 0, num;
-    int mov;
-
-    matrizResposta(matriz);
-    num = aleatorio(10,15);
-
-    while(num > 0)
-    {
-        mov = aleatorio(1,3);   // dps adicionar todos os 6 movimentos (por enquanto so temos esses)
-        if(mov != anterior)
-        {
-            realiza_mov(mov, matriz);
-            if(mov > 6)
-            {
-                realiza_mov(mov-6, matriz);    // para poder embaralhar tbm com os movimentos "inversos"
-                realiza_mov(mov-6, matriz);
-                realiza_mov(mov-6, matriz);
-            }
-            anterior = num;
-            num--;
-        }
-    }
-}
-
-void realiza_mov(int mov, int matriz[][12])
-{
-    switch(mov)
-    {
-        case 1:
-        {
-            roda_lado_direito_para_cima(matriz);
-            break;
-        }
-
-        case 2:
-        {
-            roda_lado_direito_para_baixo(matriz);
-            break;
-        }
-
-        case 3:
-        {
-            esqCimaNovo(matriz);
-            break;
-        }
-
-        case 4:
-        {
-            esqBaixo(matriz);
-            break;
-        }
-
-        case 5:
-        {
-            cimaAntiHorario(matriz);
-            break;
-        }
-
-        case 6:
-        {
-            roda_topo_do_cubo_sentido_horario(matriz);
-            break;
-        }
-
-        case 7:
-        {
-            roda_base_do_cubo_sentido_antihorario(matriz);
-            break;
-        }
-
-        // esses seriam só para o modo do jogador
-        case 8:
-        {
-            roda_base_do_cubo_sentido_horario(matriz);
-            break;
-        }
-
-        case 9:
-        {
-            roda_face_da_frente_do_cubo_sentido_horario(matriz);
-            break;
-        }
-
-        case 10:
-        {
-            roda_face_da_frente_do_cubo_sentido_antihorario(matriz);
-            break;
-        }
-
-        case 11:
-        {
-            roda_face_de_tras_do_cubo_sentido_horario(matriz);
-            break;
-        }
-
-        case 12:
-        {
-            roda_face_de_tras_do_cubo_sentido_antihorario(matriz);
-            break;
-        }
-
-        // esses seriam só para o modo do jogador
-        case 13:
-        {
-            rotacionarCima(matriz);
-            break;
-        }
-
-        case 14:
-        {
-            rotacionarBaixo(matriz);
-            break;
-        }
-
-        case 15:
-        {
-            rotacionarAntiHorario(matriz);
-            break;
-        }
-
-        case 16:
-        {
-            rotacionarHorario(matriz);
-            break;
-        }
-
-        case 17:
-        {
-            break;
-        }
-
-        case 0:
-        {
-            exit(1);
-        }
-    }
-}
-
-void menu(int matriz[][12])
-{
-    int opc, opc2;
     printf("\n\n                     BEM VINDO AO PROGRAMA (...)\n\n\n"
            "                  Pressione qualquer tecla para iniciar...");
     getch();
@@ -222,7 +38,7 @@ void menu(int matriz[][12])
     do{
         //system("cls");
         printf("\n\n                    CUBO INICIAL\n\n");
-        interfaceGrafica(matriz);
+        interfaceGrafica(cubo_magico);
         printf("\n\n"
                "                  Selecione uma das opcoes: (so opcao 1 funciona)\n"
                "                  1. Quero eu mesmo(a) montar o cubo\n"
@@ -235,48 +51,85 @@ void menu(int matriz[][12])
     {
 
         do{
-            opc = opcoesJogador(matriz);
-            realiza_mov(opc, matriz);
-        }while(!funcao_avaliadora(matriz));
+            opc = opcoesJogador(cubo_magico);
+            realiza_mov(opc, cubo_magico);
+        }while(cubo_magico);
     }
-
     system("cls");
+
+    /*if(opc == 2)
+    {
+        loopIA(f, v, cubo_magico);
+        ultimaPosicao = CuboMontado(f, caminhoAteResposta);
+        printf("Caminho ate a resposta: ");
+        for(int i = 0; i<ultimaPosicao+1; i++)
+        {
+            printf("%d - ", caminhoAteResposta[i]);
+        }
+    }*/
+    printf("\n\n         Voce venceu :) !\n");
+    interfaceGrafica(cubo_magico);
+
+
+    return 0;
 }
 
-int opcoesJogador(int matriz[2][12])
+int aleatorio(int n_min, int n_max)
+{
+    return rand() % (n_max - n_min + 1) + n_min;
+}
+
+// funcao que embaralha o cubo a partir da matriz resposta
+int cuboInicial(int v[24])
+{
+//    int anterior = 0, num;
+//    int mov;
+//
+//    matrizResposta(matriz);
+//    num = aleatorio(10,15);
+//
+//    while(num > 0)
+//    {
+//        mov = aleatorio(1,3);   // dps adicionar todos os 6 movimentos (por enquanto so temos esses)
+//        if(mov != anterior)
+//        {
+//            realiza_mov(mov, matriz);
+//            if(mov > 6)
+//            {
+//                realiza_mov(mov-6, matriz);    // para poder embaralhar tbm com os movimentos "inversos"
+//                realiza_mov(mov-6, matriz);
+//                realiza_mov(mov-6, matriz);
+//            }
+//            anterior = num;
+//            num--;
+//        }
+//    }
+    vetorResposta(v);
+}
+
+int opcoesJogador(int v[24])
 {
     int opc;
     do {
         system("cls");
         printf("\n\n                  Modo de jogo: LIVRE\n\n\n");
         printf("\n                  Cubo Atual\n");
-        interfaceGrafica(matriz);
+        interfaceGrafica(v);
         printf("\n Matriz:\n");
-        imprime(matriz);
+        imprime(v);
         printf("\n\n                  Selecione uma das acoes abaixo: (apenas 3 a 5 e 13 a 16 implementadas!!)\n"
                "                  1. Rodar lado direito para cima\n"
                "                  2. Rodar lado direito para baixo\n"
-               "                  3. Rodar lado esquerdo para cima\n"
-               "                  4. Rodar lado esquerdo para baixo\n"
-               "                  5. Rodar parte de cima no sentido anti-horario\n"
-               "                  6. Rodar parte de cima no sentido horario\n"
-               "                  7. Rodar base no sentido anti-horario\n"
-               "                  8. Rodar base no sentido horario\n"
-               "                  9. Rodar parte da frente no sentido horario\n"
-               "                  10. Rodar parte da frente no sentido anti-horario\n"
-               "                  11. Rodar parte de tras no sentido horario\n"
-               "                  12. Rodar parte de tras no sentido anti-horario\n"
-               "                  13. Rotacionar cubo para cima\n"
-               "                  14. Rotacionar cubo para baixo\n"
-               "                  15. Rotacionar cubo no sentido anti-horario\n"
-               "                  16. Rotacionar cubo no sentido horario\n"
-               "                  17. Quero que a IA resolva para mim\n"
+               "                  3. Rodar parte base no sentido anti-horario\n"
+               "                  4. Rodar parte base no sentido horario\n"
+               "                  5. Rodar parte de tras no sentido horario\n"
+               "                  6. Rodar parte de tras no sentido anti-horario"
+               "                  7. Quero que a IA resolva para mim\n"
                "                  0. Sair\n"
                "                  ");
                scanf("%d", &opc);
 
-    }while(opc<0 || opc>17);
+    }while(opc<0 || opc>7);
 
     return opc;
 }
-
